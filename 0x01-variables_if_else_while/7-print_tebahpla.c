5982d5cab98f6a83d8dc161345ba54e8b551381f
Misse335 <misse335@github.com>
2023-05-20 09:13:15 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This program prints the alphabet in lowercase in reverse
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char i;
+	char i;
+
+for (i = 'z' ; i <= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+		return (0);
+}
